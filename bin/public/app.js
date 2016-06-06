ec0dda61342502e6034993748e6c96531dce5203
Lee Elenbaas <lee@hola.org>
2016-06-06 10:36:01 +0300

    0.1.4

--- bin/public/app.js
+++ bin/public/app.js
@@ -1,42 +1,7 @@
-'use strict'; /*jslint browser:true*/
-/*global requirejs*/
-var angular_path = '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/';
-requirejs.config({
-    paths: {
-        angular: angular_path+'angular.min',
-        'angular-animate': angular_path+'angular-animate.min',
-        'angular-aria': angular_path+'angular-aria.min',
-        'angular-messages': angular_path+'angular-messages.min',
-        'angular-material':
-            '//ajax.googleapis.com/ajax/libs/angular_material/1.1.0-rc2/'
-            +'angular-material.min',
-        'angular-material-table':
-            '//cdnjs.cloudflare.com/ajax/libs/'
-            +'angular-material-data-table/0.10.8/md-data-table.min',
-        'socket.io-client': '/socket.io/socket.io'
-    },
-    shim: {
-        angular: {exports: 'angular'},
-        'angular-animate': {deps: ['angular'],
-            exports: 'angular.module("ngAnimate")'},
-        'angular-aria': {deps: ['angular'], exports: 'angular'},
-        'angular-messages': {deps: ['angular'], exports: 'angular'},
-        'angular-material': {deps: ['angular', 'angular-messages',
-            'angular-aria', 'angular-animate'], exports: 'angular'},
-        'angular-material-table': {deps: ['angular-material'],
-            exports: 'angular'}
-     },
-    map: {
-        '*': {
-            css: '//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.8/'
-            +'css.min.js'
-        }
-    }
-});
-requirejs(['angular', 'angular-material', 'health_markers/health_markers',
-    'proxies/proxies', 'css!/app'],
-function(angular){
-    var app = angular.module('lumLocal', ['ngMaterial', 'lum-health-markers',
-        'lum-proxies']);
+define(['angular', 'angular-material', 'health_markers/health_markers',
+    'proxies/proxies', 'css!/app'], function(angular){
+    'use strict'; /*jslint browser:true*/
+    var app = angular.module('lumLocal', ['ngMaterial',
+        'lum-health-markers', 'lum-proxies']);
     angular.bootstrap(document, ['lumLocal']);
 });
--- bin/public/proxies/proxies.js
+++ bin/public/proxies/proxies.js
@@ -1,15 +1,111 @@
-define(['angular', 'socket.io-client', 'angular-material-table'],
-function(angular, io){
+define(['angular', 'socket.io-client', 'angular-material-table',
+    'angular-chart'], function(angular, io){
     'use strict'; /*jslint browser:true*/
     var proxies = angular.module('lum-proxies', ['ngMaterial',
-        'md.data.table']);
+        'md.data.table', 'chart.js']);
+
+    proxies.value('lumProxyWindowConfig', {
+        refresh: 100,
+        size: 25*1000,
+        delay: 2*1000,
+        history: 50*1000
+    });
+
+    proxies.factory('lumProxyGraphOptions', proxyGraphOptions);
+    proxyGraphOptions.$inject = ['$interval', 'lumProxyWindowConfig'];
+    function proxyGraphOptions($interval, win_config){
+        var time_options = {};
+
+        var calculate_window = function(){
+            var end = Date.now() - win_config.delay;
+            var start = end - win_config.size;
+            time_options.min = start;
+            time_options.max = end;
+        };
+
+        calculate_window();
+        // XXX lee - causes browser to show as if reloading
+        // XXX lee - interval should be canceled when view does not need it
+        $interval(calculate_window, win_config.refresh);
+        return {
+            animation: {duration: 0},
+            elements: {
+                line: {borderWidth: 0.5},
+                point: {radius: 0}
+            },
+            fill: true,
+            legend: {display: false},
+            scales: {
+                xAxes: [{
+                    display: false,
+                    type: 'time',
+                    time: time_options
+                }],
+                yAxes: [{
+                    position: 'right',
+                    ticks: {
+                        min: 0,
+                        stepSize: 1,
+                        suggestedMax: 1,
+                        beginAtZero: true,
+                        callback: function(value){
+                            return Math.floor(value) == value ? value : '';
+                        }
+                    }
+                }],
+                gridLines: {display: false}
+            },
+            tooltips: {enabled: false}
+        };
+    }
 
     proxies.factory('lumProxies', proxiesService);
-    proxiesService.$inject = ['$q'];
-    function proxiesService($q){
+    proxiesService.$inject = ['$q', '$interval', 'lumProxyWindowConfig'];
+    function proxiesService($q, $interval, win_config){
         var deffered = $q.defer();
         io().on('proxies', function(proxies){
-            deffered.resolve(proxies); });
+            proxies.forEach(function(proxy){
+                proxy.stats = {
+                    hosts: [],
+                    ticks: [],
+                    active_requests: []
+                };
+            });
+            deffered.resolve(proxies); })
+        .on('stats', function(stats_chunk){
+            var now = Date.now();
+            var history_start = now - win_config.history;
+            deffered.promise.then(function(proxies){
+                Object.keys(stats_chunk).forEach(function(port){
+                    var stats = stats_chunk[port];
+                    var proxy = proxies.find(function(p){
+                        return p.port==port; });
+                    var i = proxy.stats.ticks.findIndex(function(tick){
+                        return tick >= history_start; });
+                    if (i)
+                    {
+                        proxy.stats.ticks.splice(0, i);
+                        proxy.stats.active_requests.forEach(
+                            function(active_requests){
+                                active_requests.splice(0, i); });
+                    }
+                    Object.keys(stats).forEach(function(host){
+                        if (proxy.stats.hosts.indexOf(host) === -1)
+                            proxy.stats.hosts.push(host);
+                    });
+                    proxy.stats.hosts.forEach(function(host, i){
+                        var active_requests = proxy.stats.active_requests[i];
+                        if (!active_requests)
+                        {
+                            active_requests = proxy.stats.active_requests[i]
+                                = [];
+                        }
+                        active_requests.push(stats[host].active_requests);
+                    });
+                    proxy.stats.ticks.push(now);
+                });
+            });
+        });
         return deffered.promise;
     }
 
@@ -28,12 +124,14 @@ function(angular, io){
     ]);
 
     proxies.controller('ProxiesTable', ProxiesTableController);
-    ProxiesTableController.$inject=['lumProxies', 'lumOptColumns'];
-    function ProxiesTableController(lum_proxies, opt_columns){
+    ProxiesTableController.$inject=['lumProxies', 'lumOptColumns',
+        'lumProxyGraphOptions'];
+    function ProxiesTableController(lum_proxies, opt_columns, graph_options){
         var $vm = this;
         $vm.resolved = false;
         $vm.proxies = [];
         $vm.opt_columns = [];
+        $vm.graph_options = graph_options;
         lum_proxies.then(function(proxies){
             $vm.resolved = true;
             $vm.proxies = proxies;
